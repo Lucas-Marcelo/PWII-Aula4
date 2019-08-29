6dc1d52e2eb710254da9d33c4ce1bbd6520a5338
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-08-28 21:19:55 -0300

    Criação da Classe Viagem

--- viagem.ts
+++ viagem.ts
@@ -1,12 +1,12 @@
 class Viagem{
     constructor(public tempoGasto:number, public velocidadeMedia:number, public rendimentoVeiculo:number) {}
 
-    distancia(){
+    distancia(): number{
         return this.tempoGasto * this.velocidadeMedia
         //console.log(`Distância: ${this.tempoGasto * this.velocidadeMedia}`)
     }
 
-    consumo () {
+    consumo (): number {
         return this.distancia() / this.rendimentoVeiculo
         //console.log(`Consumo: ${this.distancia() / this.rendimentoVeiculo}`)
     }
